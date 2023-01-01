044ca43000b73b1c7bc0203d380b19e287bc136e
Ahmed Rachid <ahazourli@ahazourli365JV.vmware.com>
2023-01-02 00:25:31 +0100

    added greenplumpython API

---
+++ db/api.py
@@ -0,0 +1,40 @@
+import pathlib
+import greenplumpython as gp
+
+
+db = gp.database(
+        host="35.241.130.38",
+        dbname="dev",
+        user="gpadmin",
+        password="79kq97lCaWrFn",
+    )
+
+def get_wind_data(start, end):
+    """
+    Query wind data rows between two ranges
+
+    :params start: start row id
+    :params end: end row id
+    :returns: pandas dataframe object
+    """
+
+    df  = db.table('wind_speed_generation')
+    df = df[lambda t: t["index"] > start and t['index'] <  end ]
+    df = df.to_dataframe()
+
+    return df
+
+
+def get_wind_data_by_id(id):
+    """
+    Query a row from the Wind Table
+
+    :params id: a row id
+    :returns: pandas dataframe object
+    """
+
+    df = db.table('wind_speed_generation')
+    df = df[lambda t: t["index"] == id ]
+    df = df.to_dataframe()
+
+    return df
