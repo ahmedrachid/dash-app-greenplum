c6e3789950fd72e5ba5318993716d24412faefa8
Ahmed Rachid <ahmedrachidhazourli@yahoo.fr>
2023-01-09 16:28:33 +0100

    Add files via upload

--- db/api.py
+++ db/api.py
@@ -1,13 +1,21 @@
 import pathlib
-import greenplumpython as gp
+import pandas as pd
+from sqlalchemy import create_engine
 
+con  = create_engine('postgresql+psycopg2://gpadmin:changeme@10.208.134.51/test2')
+#db = gp.database(
+#        host="35.241.130.38",
+#        dbname="dev",
+#        user="gpadmin",
+#        password="79kq97lCaWrFn",
+#    )
 
-db = gp.database(
-        host="",
-        dbname="dev",
-        user="gpadmin",
-        password="",
-    )
+#db = gp.database(
+#        host="10.208.134.51",
+#        dbname="test2",
+#        user="gpadmin",
+#        password="changeme",
+#    )
 
 def get_wind_data(start, end):
     """
@@ -17,12 +25,24 @@ def get_wind_data(start, end):
     :params end: end row id
     :returns: pandas dataframe object
     """
-
-    df  = db.table('wind_speed_generation')
-    df = df[lambda t: t["index"] > start and t['index'] <  end ]
-    df = df.to_dataframe()
-
+    print(start, end)
+    statement = f'SELECT index,speed, speederror, direction FROM wind_speed WHERE index > {start} AND index <= {end};'
+    df = pd.read_sql_query(statement, con)
+    print(len(df))
     return df
+
+def get_data_count():
+    """
+    Query wind data rows between two ranges
+
+    :params start: start row id
+    :params end: end row id
+    :returns: pandas dataframe object
+    """
+    statement = f'SELECT count(*) FROM wind_speed;'
+    df = pd.read_sql_query(statement, con)
+    return df
+
 
 
 def get_wind_data_by_id(id):
@@ -33,8 +53,8 @@ def get_wind_data_by_id(id):
     :returns: pandas dataframe object
     """
 
-    df = db.table('wind_speed_generation')
-    df = df[lambda t: t["index"] == id ]
-    df = df.to_dataframe()
 
+    statement = f'SELECT index,speed, speederror, direction FROM wind_speed WHERE index = {id};'
+    df = pd.read_sql_query(statement, con)
     return df
+
